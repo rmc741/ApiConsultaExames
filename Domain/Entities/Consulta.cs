98896bfbf8ec9011b9f1a4ca422ba44a5abd939a
RafaelMendonca <rafael.medonca2@gmail.com>
2023-07-18 08:45:24 -0300

    Corrigido validação errada.

--- Domain/Entities/Consulta.cs
+++ Domain/Entities/Consulta.cs
@@ -23,7 +23,7 @@ public class Consulta : EntityBase
         DomainValidationException.When(string.IsNullOrEmpty(descricao), "Descrição não pode ser vazio ou nulo!");
         DomainValidationException.When(descricao.Length < 3, "Descrição precisar ter pelo menos 3 caracteres!");
 
-        DomainValidationException.When(DataConsulta <= DateTime.Now, "Necessário uma data válida para consulta!");
+        DomainValidationException.When(dataConsulta <= DateTime.Now, "Necessário uma data válida para consulta!");
 
         DomainValidationException.When(string.IsNullOrEmpty(medicoCRM), "Necessario informar CRM do médico!");
 
