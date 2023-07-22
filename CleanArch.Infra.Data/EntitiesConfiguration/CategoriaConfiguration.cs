bf33c1f4c97eacedb623887d1b47e2ee3dc7a44d
Rafael Mendonca <rafael.mendonca2@gmail.com>
2023-07-22 15:16:45 -0300

    Iniciado configurações de propriedades das entidaes.

--- CleanArch.Infra.Data/EntitiesConfiguration/CategoriaConfiguration.cs
+++ CleanArch.Infra.Data/EntitiesConfiguration/CategoriaConfiguration.cs
@@ -1,5 +1,15 @@
-﻿namespace CleanArch.Infra.Data.EntitiesConfiguration;
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CategoriaConfiguration
+namespace CleanArch.Infra.Data.EntitiesConfiguration;
+
+public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
 {
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(p => p.Nome).HasMaxLength(100).IsRequired();
+        builder.Property(p => p.TipoCategoria).HasMaxLength(100).IsRequired();
+    }
 }
--- CleanArch.Infra.Data/EntitiesConfiguration/ConsultaConfiguration.cs
+++ CleanArch.Infra.Data/EntitiesConfiguration/ConsultaConfiguration.cs
@@ -1,5 +1,20 @@
-﻿namespace CleanArch.Infra.Data.EntitiesConfiguration;
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ConsultaConfiguration
+namespace CleanArch.Infra.Data.EntitiesConfiguration;
+
+public class ConsultaConfiguration : IEntityTypeConfiguration<Consulta>
 {
+    public void Configure(EntityTypeBuilder<Consulta> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Descricao).HasMaxLength(255).IsRequired();
+        builder.Property(x => x.DataConsulta).IsRequired();
+        builder.Property(x => x.MedicoCRM).HasMaxLength(100).IsRequired();
+        builder.Property(x =>x.UsuarioId).IsRequired();
+        builder.Property(x =>x.CategoriaId).IsRequired();
+
+        builder.HasOne(x => x.Categoria).WithMany(x => x.UsuarioConsultas).HasForeignKey(x => x.CategoriaId);
+    }
 }
