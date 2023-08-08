7dcb90024ccc5f96f3bc0b3ae8391e589ff7c187
RafaelMendonca <rafael.medonca2@gmail.com>
2023-08-08 11:54:53 -0300

    Adicionado usuario DTO

--- CleanArch.Application/DTOs/UsuarioDTO.cs
+++ CleanArch.Application/DTOs/UsuarioDTO.cs
@@ -1,5 +1,40 @@
-﻿namespace CleanArch.Application.DTOs;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArch.Application.DTOs;
 
 public class UsuarioDTO
 {
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(255)]
+    [DisplayName("NomeUsuario")]
+    public string NomeUsuario { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(255)]
+    [DisplayName("NomeCompleto")]
+    public string NomeCompleto { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(255)]
+    [DisplayName("Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(6)]
+    [MaxLength(255)]
+    [DisplayName("Password")]
+    public string Password { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(50)]
+    [DisplayName("Situacao")]
+    public string Situacao { get; set; }
+
 }
