46c42fddfe67df44ed36dcdc79c6f8605d0b6f2e
RafaelMendonca <rafael.medonca2@gmail.com>
2023-08-09 14:25:50 -0300

    Incluso ExameDTO

--- CleanArch.Application/DTOs/ExameDTO.cs
+++ CleanArch.Application/DTOs/ExameDTO.cs
@@ -1,5 +1,36 @@
-﻿namespace CleanArch.Application.DTOs;
+﻿using CleanArch.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CleanArch.Application.DTOs;
 
 public class ExameDTO
 {
+    [Required(ErrorMessage = "Descrição é Necessária")]
+    [MinLength(3)]
+    [MaxLength(255)]
+    [DisplayName("Descricao")]
+    public string Descricao { get; set; }
+
+    [Required]
+    [DisplayName("DataExame")]
+    [DataType(DataType.DateTime)]
+    public DateTime DataExame { get; set; }
+
+    [Required]
+    [DisplayName("MedicoCRM")]
+    public string MedicoCRM { get; set; }
+
+    [Required]
+    [DisplayName("UsuarioId")]
+    public int UsuarioId { get; set; }
+    [JsonIgnore]
+    public Usuario Usuario { get; set; }
+
+    [Required]
+    [DisplayName("CategoriaId")]
+    public int CategoriaId { get; set; }
+    [JsonIgnore]
+    public Categoria Categoria { get; set;}
 }
